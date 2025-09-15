dd98587733ca04e28216de211d021dedb616be59
Jarvis Falia <nocodyca@gmail.com>
2025-09-15 15:55:48 -0400

    script edits

---
+++ class-demos/week_02/script.js
@@ -0,0 +1,7 @@
+// this is a comment in javascript      
+
+// this is loading event with anon function
+window.onload = () => {
+    alert("page is loaded");
+    console.log ("page is fully loaded");
+}
