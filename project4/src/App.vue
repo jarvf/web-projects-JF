be5eb41a8b16b468dd5b65a49880bd8a289768d1
Jarvis Falia <nocodyca@gmail.com>
2025-11-16 22:39:26 -0500

    added base layout, global styles

---
+++ project4/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="app-shell">
+    <header>
+      <h1 class="rainbow-text">CyberDream 2000™</h1>
+      <p class="blink">THE FUTURE IS NOW\!</p>
+    </header>
+
+    <main>
+      <p>
+        Experimental ad site for my Project 4 starts as a cheesy 90s infomercial,
+        then slowly falls apart the more you interact with it.
+      </p>
+      <p>
+        first I’m wiring up layout + global styles, then creepy behaviors.
+      </p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppShell'
+}
+</script>
